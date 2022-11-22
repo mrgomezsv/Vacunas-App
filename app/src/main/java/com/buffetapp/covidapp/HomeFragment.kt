900b7cc30f9874eb2f0a0c1eaab709e01c19353a
Mario Roberto <mrgomez.dev@gmail.com>
2022-11-21 21:02:55 -0600

    HomeFragment y prueba ok

--- app/src/main/java/com/buffetapp/covidapp/HomeFragment.kt
+++ app/src/main/java/com/buffetapp/covidapp/HomeFragment.kt
@@ -23,13 +23,18 @@ class Home : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private var layoutManager: RecyclerView.LayoutManager? = null //Jalamos el LayoutManager como global
+    private lateinit var adapter: NewsAdapter
+    val newsList = ArrayList<News>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
+
         }
+        createData()
     }
 
     override fun onCreateView(
@@ -39,13 +44,15 @@ class Home : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-//Declaramos el recyclerView
+    /**********************************************************************/
+    //Declaramos el recyclerView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = view.findViewById<RecyclerView>(R.id.lista_recyclerview)
         layoutManager = LinearLayoutManager(view.context,LinearLayoutManager.VERTICAL,false)
         recyclerView.layoutManager = layoutManager
-
+        adapter = NewsAdapter(newsList)
+        recyclerView.adapter = adapter
     }
 
     companion object {
@@ -66,5 +73,18 @@ class Home : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }*/
+    }
+
+    private fun createData(){
+        newsList.add(News(1,R.drawable.img,"Titulo de la Noticia","Este es el resumen de la noticia que queresmos ver mas adealnte con respecto a la vacunacion.."))
+        newsList.add(News(2,R.drawable.img,"Titulo de la Noticia","Este es el resumen de la noticia que queresmos ver mas adealnte con respecto a la vacunacion.."))
+        newsList.add(News(3,R.drawable.img,"Titulo de la Noticia","Este es el resumen de la noticia que queresmos ver mas adealnte con respecto a la vacunacion.."))
+        newsList.add(News(4,R.drawable.img,"Titulo de la Noticia","Este es el resumen de la noticia que queresmos ver mas adealnte con respecto a la vacunacion.."))
+        newsList.add(News(5,R.drawable.img,"Titulo de la Noticia","Este es el resumen de la noticia que queresmos ver mas adealnte con respecto a la vacunacion.."))
+        newsList.add(News(6,R.drawable.img,"Titulo de la Noticia","Este es el resumen de la noticia que queresmos ver mas adealnte con respecto a la vacunacion.."))
+        newsList.add(News(7,R.drawable.img,"Titulo de la Noticia","Este es el resumen de la noticia que queresmos ver mas adealnte con respecto a la vacunacion.."))
+        newsList.add(News(8,R.drawable.img,"Titulo de la Noticia","Este es el resumen de la noticia que queresmos ver mas adealnte con respecto a la vacunacion.."))
+        newsList.add(News(9,R.drawable.img,"Titulo de la Noticia","Este es el resumen de la noticia que queresmos ver mas adealnte con respecto a la vacunacion.."))
+        newsList.add(News(10,R.drawable.img,"Titulo de la Noticia","Este es el resumen de la noticia que queresmos ver mas adealnte con respecto a la vacunacion.."))
     }
 }
--- app/src/main/java/com/buffetapp/covidapp/NewsAdapter.kt
+++ app/src/main/java/com/buffetapp/covidapp/NewsAdapter.kt
@@ -1,11 +1,36 @@
 package com.buffetapp.covidapp
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NewsAdapter {
+class NewsAdapter(val newsList: ArrayList<News>):RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
+        fun bindItems(news:News){
+            val image = itemView.findViewById<ImageView>(R.id.newImage)
+            val id = itemView.findViewById<TextView>(R.id.new_id)
+            val title = itemView.findViewById<TextView>(R.id.title)
+            val resume = itemView.findViewById<TextView>(R.id.news_resume)
+
+
+            title.text = news.title.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.news_item,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItems(newsList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return newsList.size
     }
 }
