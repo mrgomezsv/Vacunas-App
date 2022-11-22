d1894cf75d1c501e459bb7571c023d20859096e3
Mario Roberto <mrgomez.dev@gmail.com>
2022-11-21 20:07:43 -0600

    Home RecyclerView insertado

--- app/src/main/java/com/buffetapp/covidapp/HomeFragment.kt
+++ app/src/main/java/com/buffetapp/covidapp/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,7 @@ class Home : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null //Jalamos el LayoutManager como global
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +38,14 @@ class Home : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+//Declaramos el recyclerView
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.lista_recyclerview)
+        layoutManager = LinearLayoutManager(view.context,LinearLayoutManager.VERTICAL,false)
+        recyclerView.layoutManager = layoutManager
+
     }
 
     companion object {
